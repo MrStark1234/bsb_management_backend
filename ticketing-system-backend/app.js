a4458e6ee56246cf0b91cd7c5fe357a556d7667b
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2023-09-21 17:29:42 +0530

    resolving cors error

--- ticketing-system-backend/app.js
+++ ticketing-system-backend/app.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
 
 //Routes
 const calenderRoutes = require("./routes/calender");
@@ -30,65 +29,71 @@ const uploadTemplateRoutes = require("./routes/uploadTemplate");
 const ticketingRoutes = require("./routes/ticketing");
 const fireProtectionReportRoutes = require("./routes/fireProtectionReport");
 //Connection
-mongoose.connect(process.env.DATABASE,{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true
-}).then(()=>{
-    console.log('DATA CONNECTED');
-}).catch((err)=>{
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DATA CONNECTED");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.use(express.static('uploads')); 
-app.use('/uploads', express.static('uploads'));
-app.use(express.static('uploads/location')); 
-app.use('/uploads/location', express.static('uploads/location'));
-app.use(express.static('uploads/documents')); 
-app.use('/uploads/documents', express.static('uploads/documents'));
-app.use(express.static('uploads/plan')); 
-app.use('/uploads/plan', express.static('uploads/plan'));
-app.use(express.static('uploads/sidebar')); 
-app.use('/uploads/sidebar', express.static('uploads/sidebar'));
-app.use(express.static('uploads/calender')); 
-app.use('/uploads/calender', express.static('uploads/calender'));
-app.use(express.static('uploads/releaseform')); 
-app.use('/uploads/releaseform', express.static('uploads/releaseform'));
-app.use(express.static('uploads/template')); 
-app.use('/uploads/template', express.static('uploads/template'));
-app.use(express.static('uploads/ticketing')); 
-app.use('/uploads/ticketing', express.static('uploads/ticketing'));
+app.use(express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
+app.use(express.static("uploads/location"));
+app.use("/uploads/location", express.static("uploads/location"));
+app.use(express.static("uploads/documents"));
+app.use("/uploads/documents", express.static("uploads/documents"));
+app.use(express.static("uploads/plan"));
+app.use("/uploads/plan", express.static("uploads/plan"));
+app.use(express.static("uploads/sidebar"));
+app.use("/uploads/sidebar", express.static("uploads/sidebar"));
+app.use(express.static("uploads/calender"));
+app.use("/uploads/calender", express.static("uploads/calender"));
+app.use(express.static("uploads/releaseform"));
+app.use("/uploads/releaseform", express.static("uploads/releaseform"));
+app.use(express.static("uploads/template"));
+app.use("/uploads/template", express.static("uploads/template"));
+app.use(express.static("uploads/ticketing"));
+app.use("/uploads/ticketing", express.static("uploads/ticketing"));
 
 const port = process.env.PORT || 8000;
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 //My Routes
-app.use('/api',calenderRoutes);
-app.use('/api',sidebarRoutes);
-app.use('/api',authRoutes);
-app.use('/api',stripeRoutes);
-app.use('/api',dragRoutes);
-app.use('/api',userRoutes);
-app.use('/api',packageRoutes);
-app.use('/api',menuDropdownRoutes);
-app.use('/api',locationRoutes);
-app.use('/api',categoryRoutes);
-app.use('/api',documentManagementRoutes);
-app.use('/api',plancategoryRoutes);
-app.use('/api',planRoutes);
-app.use('/api',officerRoutes);
-app.use('/api',formRoutes);
-app.use('/api',evacuationRoutes);
-app.use('/api',releaseFormPartBRoutes);
-app.use('/api',releaseFormPartCRoutes);
-app.use('/api',uploadTemplateRoutes);
-app.use('/api',fireProtectionReportRoutes);
+app.use("/api", calenderRoutes);
+app.use("/api", sidebarRoutes);
+app.use("/api", authRoutes);
+app.use("/api", stripeRoutes);
+app.use("/api", dragRoutes);
+app.use("/api", userRoutes);
+app.use("/api", packageRoutes);
+app.use("/api", menuDropdownRoutes);
+app.use("/api", locationRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", documentManagementRoutes);
+app.use("/api", plancategoryRoutes);
+app.use("/api", planRoutes);
+app.use("/api", officerRoutes);
+app.use("/api", formRoutes);
+app.use("/api", evacuationRoutes);
+app.use("/api", releaseFormPartBRoutes);
+app.use("/api", releaseFormPartCRoutes);
+app.use("/api", uploadTemplateRoutes);
+app.use("/api", fireProtectionReportRoutes);
 //app.use('/api',ticketingRoutes);
 
-app.listen(port,()=>{
-    console.log(`Server is running at port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
 });
-
